5bbb2d5d150ac15d26d6aa9b3a9aea3493c50c09
Brooke500 <b.a.scott@wustl.edu>
2025-10-01 14:10:20 -0500

    Studio 4 Done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,31 @@
 package studio4;
 
 import java.awt.Color;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		JFileChooser chooser = new JFileChooser("resources");
+		chooser.showOpenDialog(null);
+		File f = new File(chooser.getSelectedFile().getPath());
+		Scanner in = new Scanner(f); //making Scanner with a File
+
+		StdDraw.clear();
+
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.filledRectangle(0.5,0.5,1,1);
+		StdDraw.setPenColor(146,0,0);
+		StdDraw.filledRectangle(0.5, 0.5, 0.2, 0.2);
+
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledCircle(0.5,0.5,0.1);
+
 		
 	}
 }
